079b30f530c1d0ec7171f5f6172f2e8c2705981b
kynetiv <miles@kynetiv.com>
2017-01-10 19:00:21 -0800

    wip add fotw redirects and scripts

--- js/src/browserify-args.sh
+++ js/src/browserify-args.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo '-d -s picc -p [ minifyify --map picc.js.map --output js/picc.js.map --uglify [ --no-mangle ] ] -o js/picc.js js/src/picc.js'
+echo '-d -s fotw -p [ minifyify --map fotw.js.map --output js/fotw.js.map --uglify [ --no-mangle ] ] -o js/fotw.js js/src/fotw.js'
