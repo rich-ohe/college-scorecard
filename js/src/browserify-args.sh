3d9630b65f5499820bde673df15d47aba8663b6a
kynetiv <miles@kynetiv.com>
2017-01-27 13:55:04 -0500

    rename fotw redirect script

--- js/src/browserify-args.sh
+++ js/src/browserify-args.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo '-d -s picc -p [ minifyify --map picc.js.map --output js/picc.js.map --uglify [ --no-mangle ] ] -o js/picc.js js/src/picc.js'
-echo '-d -s fotw -p [ minifyify --map fotw.js.map --output js/fotw.js.map --uglify [ --no-mangle ] ] -o js/fotw.js js/src/fotw.js'
+echo '-d -s fotw -p [ minifyify --map index.fotw.js.map --output js/index.fotw.js.map --uglify [ --no-mangle ] ] -o js/index.fotw.js js/src/index.fotw.js'
