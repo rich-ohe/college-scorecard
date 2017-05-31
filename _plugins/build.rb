f56280b41bf859fa5aee6850732863b264927942
Miles Fink <miles@kynetiv.com>
2017-05-31 15:37:55 -0400

    trigger rebuild

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -1,3 +1,3 @@
 # Build assets for all branches
-puts '[build.rb] npm install and browersify assets'
+puts '[build.rb] npm install and rebuild and browersify assets'
 `(npm install; npm rebuild; npm run build) &> build.log`
